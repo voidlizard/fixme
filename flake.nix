c95bcbfd28ea2094900ba166dc6a4b737cb8b3e0
Dmitry Zuikov <dzuikov@gmail.com>
2023-02-07 12:28:31 +0300

    wip

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+description = "fixme: trackerless issue managament";
+
+inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    haskell-flake-utils.url = "github:ivanovs-4/haskell-flake-utils";
+    suckless-conf.url = "github:voidlizard/suckless-conf";
+};
+
+outputs = { self, nixpkgs, haskell-flake-utils, ... }@inputs:
+    haskell-flake-utils.lib.simpleCabal2flake {
+      inherit self nixpkgs;
+      # systems = [ "x86_64-linux" ];
+
+      name = "fixme";
+
+      haskellFlakes = [ inputs.suckless-conf ];
+
+      packagePostOverrides = { pkgs }: with pkgs; with haskell.lib; [
+       disableExecutableProfiling
+       disableLibraryProfiling
+       dontBenchmark
+       dontCoverage
+       dontDistribute
+       dontHaddock
+       dontHyperlinkSource
+       doStrip
+       enableDeadCodeElimination
+       justStaticExecutables
+
+       dontCheck
+      ];
+
+      # Additional build intputs of the default shell
+      shellExtBuildInputs = {pkgs}: with pkgs; [
+        haskellPackages.haskell-language-server
+      ];
+
+      # Wether to build hoogle in the default shell
+      # shellWithHoogle = true;
+
+    };
+}
