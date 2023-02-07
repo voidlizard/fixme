28160dec82b715c86450230cd579df6885773242
Dmitry Zuikov <dzuikov@gmail.com>
2023-02-07 16:05:34 +0300

    starting version

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,6 @@
 {-# OPTIONS_GHC -fno-warn-type-defaults #-}
 {-# Language PatternSynonyms #-}
+{-# Language TemplateHaskell #-}
 module Main where
 
 import Fixme.Defaults
@@ -10,26 +11,61 @@ import Data.Config.Suckless
 import Data.Text (Text)
 import Data.Text qualified as Text
 import Data.Text.IO qualified as Text
+import Data.String
 
-import Text.InterpolatedString.Perl6 (qc)
 import Control.Monad
-import Options.Applicative
+import Data.Attoparsec.Text
+import Data.Attoparsec.Text qualified as Atto
+import Data.Foldable (for_)
+import Data.Generics.Uniplate.Data()
+import Data.List qualified as List
+import Options.Applicative hiding (Parser)
+import Options.Applicative qualified as O
 import Prettyprinter
 import System.Directory
-import System.FilePath.Posix
-import Data.Either
-import Data.Data
-
 import System.FilePattern.Directory
+import Control.Concurrent.Async
 
-import Data.Generics.Uniplate.Data()
-import Data.Generics.Uniplate.Operations
-
-
-type C = MegaParsec
+import Lens.Micro.Platform
 
 pattern Key :: forall {c}. Id -> [Syntax c] -> [Syntax c]
 pattern Key n ns <- SymbolVal  n : ns
+
+type C = MegaParsec
+
+newtype FixmeHeader =
+  FixmeHeader
+  { fixmeIndent :: Int
+  }
+
+data FixmeDef =
+  FixmeDef
+  { _fixmeComm :: [Text]
+  , _fixmeTags :: [Text]
+  }
+
+makeLenses 'FixmeDef
+
+type FixmeTitle = Text
+type FixmeTag = Text
+
+data Fixme =
+  Fixme
+  { _fixmeTag   :: FixmeTag
+  , _fixmeTitle :: FixmeTitle
+  , _fixmeFile  :: FilePath
+  , _fixmeLine  :: Int
+  , _fixmeIdent :: Int
+  }
+  deriving stock (Show)
+
+makeLenses 'Fixme
+
+newtype Brief a = Brief a
+
+instance Pretty (Brief Fixme) where
+  pretty (Brief f) = pretty (view fixmeTag f)
+                      <+> pretty (Text.take 50 $ view fixmeTitle f)
 
 runInit  :: IO ()
 runInit = do
@@ -42,8 +78,8 @@ runInit = do
   unless confExists do
     Text.writeFile confFile defConfig
 
-runCheck :: IO ()
-runCheck = do
+runScan :: IO ()
+runScan = do
 
   cfgFile <- readFile confFile
 
@@ -58,18 +94,56 @@ runCheck = do
                       | (ListVal @C (Key "fixme-files-ignore" fs) ) <- r
                       ]
 
+  let comm = mconcat [ fmap (fromString.show.pretty) fs
+                     | (ListVal @C (Key "fixme-comments" fs) ) <- r
+                     ]
+
+  let pref = mconcat [ fmap (fromString.show.pretty) fs
+                     | (ListVal @C (Key "fixme-prefix" fs) ) <- r
+                     ]
+
   files <- getDirectoryFilesIgnore "." masks ignore
 
-  -- print $ pretty masks
 
-  mapM_ parseFile files
+  let fxdef = FixmeDef (List.nub comm)  (List.nub ("FIXME:" : pref))
 
-parseFile :: FilePath -> IO ()
-parseFile fp = do
-  putStrLn fp
+  fme <- mconcat <$> mapConcurrently (parseFile fxdef) files
+
+  mapM_ (print.pretty.Brief) fme
+
+
+parseFile :: FixmeDef -> FilePath -> IO [Fixme]
+parseFile def fp = do
+  -- hPutStrLn stderr fp
   -- FIXME: check if file is too big
-  txt <- Text.readFile fp
-  pure ()
+  txt <- Text.readFile fp <&> Text.lines
+                          <&> zip [0..]
+
+  r <- forM txt $ \(i,s) -> do
+        let fixme0 = Fixme "" "" fp i 0
+        let fixme = parseOnly (pHeader def fixme0) s
+        case fixme of
+          Left{}  -> pure mempty
+          Right h -> pure [h]
+
+  pure (mconcat r)
+
+pHeader :: FixmeDef -> Fixme -> Parser Fixme
+pHeader fx fme = do
+
+  spaces <- Atto.takeWhile isHorizontalSpace
+  _      <- optional (choice comm)
+  _      <- Atto.skipWhile isHorizontalSpace
+  tag    <- choice tags
+  _      <- Atto.skipWhile isHorizontalSpace
+  title  <- Text.pack <$> Atto.manyTill anyChar (endOfInput <|> endOfLine)
+
+  pure $  fme & set fixmeTag   tag
+              & set fixmeTitle title
+
+  where
+    comm = fmap string (view fixmeComm fx)
+    tags = fmap string (view fixmeTags fx)
 
 
 main :: IO ()
@@ -80,14 +154,14 @@ main = join . customExecParser (prefs showHelpOnError) $
   <> progDesc "trackerless issue management"
   )
   where
-    parser ::  Parser (IO ())
+    parser ::  O.Parser (IO ())
     parser = hsubparser (  command "init" (info pInit (progDesc "init fixme config"))
-                        <> command "check" (info pCheck (progDesc "check"))
+                        <> command "scan" (info pScan (progDesc "scan"))
                         )
     pInit = do
       pure runInit
 
-    pCheck = do
-      pure runCheck
+    pScan = do
+      pure runScan
 
 
